8bef94536fc05988da90c47ec46d389535f6f5a7
AlexandraCynthia <alexandracynthia1663@gmail.com>
2024-05-22 22:08:23 -0500

    validación de formulario

--- js/controllers.js/main.js
+++ js/controllers.js/main.js
@@ -56,15 +56,53 @@ const render = async()=>{
     }
 };
 
+
+    const nameInput = document.querySelector("[data-name]");
+    const priceInput = document.querySelector("[data-price]");
+    const imageInput = document.querySelector("[data-image]");
+
+    nameInput.addEventListener("input", () => clearErrorMessage(nameInput));
+    priceInput.addEventListener("input", () => clearErrorMessage(priceInput));
+    imageInput.addEventListener("input", () => clearErrorMessage(imageInput));
+
+
+
 form.addEventListener("submit", async(event)=>{
     event.preventDefault();
 
-    const name = document.querySelector("[data-name]").value;
-    const price = document.querySelector("[data-price]").value;
-    const image = document.querySelector("[data-image]").value;
+    const name = nameInput.value.trim();
+    const price = priceInput.value.trim();
+    const image = imageInput.value.trim();
 
-    try{
+    //Limpiar mensajes de error anteriores
+    clearErrorMessage(nameInput);
+    clearErrorMessage(priceInput);
+    clearErrorMessage(imageInput);
+
+    //Verifica si algún campo está vacio
+
+    let isValid = true;
+    if (!name){
+        displayErrorMessage(nameInput, "Por favor, completa este campo.");
+        isValid=false;
+    }
+    if(!price){
+        displayErrorMessage(priceInput, "Por favor, completa este campo.");
+        isValid=false;
+    }
+    if(!image){
+        displayErrorMessage(imageInput, "Por favor, completa este campo.");
+        isValid=false;
+    }
+
+    if (!isValid){
+        return; 
+    }
+
+    try {
         const newProduct = await serviceProducts.createProducts(name, price, image);
+
+        //Si se crea el producto exitosamente, crear la tarjeta y limpiar formulario
         if(newProduct){
             createCard(newProduct.name, newProduct.price, newProduct.image, newProduct.id);
             form.reset();
@@ -74,5 +112,22 @@ form.addEventListener("submit", async(event)=>{
     }
 })
 
+function displayErrorMessage(input, message){
+    let errorMessage = input.nextElementSibling;
+    if(!errorMessage || !errorMessage.classList.contains("error-message")){
+        errorMessage = document.createElement("span");
+        errorMessage.classList.add("error-message");
+        input.parentNode.insertBefore(errorMessage, input.nextElementSibling)
+    }
+    errorMessage.textContent = message;
+   
+}
+
+function clearErrorMessage(input){
+    const errorMessage = input.nextElementSibling;
+    if(errorMessage && errorMessage.classList.contains("error-message")){
+        errorMessage.remove();
+    }
+}
 
 render();

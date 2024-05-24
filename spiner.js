9ecbf77a8b969e05968fd7d14ea85b61f3cbb943
AlexandraCynthia <alexandracynthia1663@gmail.com>
2024-05-23 21:12:48 -0500

    corrigiendo CSS

--- spiner.js
+++
@@ -1,5 +0,0 @@
-window.onload = function(){
-    var contenedor = document.getElementsByClassName('products-container');
-    contenedor.style.visibility = 'hidden';
-    contenedor.style.opacity = '0';
-}
